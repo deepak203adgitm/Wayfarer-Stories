dd6911026b052b8d72b3e3c7d4c925401ce3b8ee
deepak.c.sharma <deepak.c.shrma@ericsson.com>
2026-04-14 18:12:26 +0000

    v2

---
+++ coder-sorcerer-site-deploy/sw.js
@@ -0,0 +1,17 @@
+var CACHE = 'cs-v2';
+var FILES = ['./index.html', './css/style.css', './js/config.js', './js/app.js', './js/games.js'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+  }));
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+});
